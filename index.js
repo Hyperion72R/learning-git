b67ab0f08c81c78866136d0a82f8ca91023652a5
Hyperion72R <83410226+Hyperion72R@users.noreply.github.com>
2021-05-02 14:03:23 +0200

    Added timeout function

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let tog = document.getElementById("tog");
 tog.addEventListener("click", function() {
   tog.classList.toggle("m-fadeOut");
 });
+
+
+function Timeout() {
+  setTimeout(function() {
+    tog.classList.toggle("btnTest");
+}, 500) }
